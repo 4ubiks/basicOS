d1835968efc0c0e4d7f4252209221ecdac68b71c
4ubiks <144192094+4ubiks@users.noreply.github.com>
2025-01-02 18:14:41 -0700

    isr_install fixed, still isn't being called

--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -1,11 +1,15 @@
 #include "../drivers/screen.h"
 #include "util.h"
+#include "../cpu/isr.h"
+#include "../cpu/idt.h"
 
 int main(){
 	//char* video_memory = (char*) 0xb8000;
 	//*video_memory = 'X';
-	clear_screen();
 
+	// before system interrupts implemented. 
+	// old code!
+/*	clear_screen();
 
 	int i=0;
 	for (i=0; i < 24; i++){
@@ -16,6 +20,11 @@ int main(){
 
 	kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
 	kprint("With this text, the kernel will scroll again, making row 1 also disappear");
-	
+*/
+
+	isr_install();
+
+	__asm__ __volatile__("int $2");
+		
 	return 0;
 }
